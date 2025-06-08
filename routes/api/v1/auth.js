155fd3e96881fb1d02a8a0bcfb57b309e9b2e600
AmineElb1 <r0934310@student.thomasmore.be>
2025-06-08 19:39:20 +0200

    Add reset password routes to auth router

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { loginUser, getCurrentUser } = require("../../../controllers/auth");
+const { loginUser, getCurrentUser, requestPasswordReset, resetPassword } = require("../../../controllers/auth");
 const authMiddleware = require("../../../middleware/auth"); // Middleware toevoegen
 
 router.post("/login", loginUser);
 router.get("/me", authMiddleware, getCurrentUser); // Auth middleware toevoegen
+router.post("/reset-password", requestPasswordReset);
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
