2312d4605267c40778a411232ac9b519f70794ab
Rocky Lai <99881874+WingLai852@users.noreply.github.com>
2025-06-08 17:18:58 +0200

    kleine fix

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../../../controllers/users'); // Adjust path as needed
+const { authenticate } = require('../../../middleware/auth'); // Adjust path as needed
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// PATCH /api/v1/users/:id - Update een bestaande user
-router.patch("/:id", usersController.updateUser.updateMe);
+// PATCH /api/v1/users/me - Update the current user
+router.patch("/me", authenticate, usersController.updateMe);
+
 
 module.exports = router;
